7d47cdc68d698971a4c1f8033312a450c99de35f
tggKim <138564756+tggKim@users.noreply.github.com>
2024-01-16 20:54:58 +0900

    Delete springbootdeveloper directory

--- springbootdeveloper/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO article (title,content,created_at,updated_at) VALUES ('제목1','내용 1',NOW(),NOW())
-INSERT INTO article (title,content,created_at,updated_at) VALUES ('제목2','내용 2',NOW(),NOW())
